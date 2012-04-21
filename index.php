decf223e712d70ae5b07760be1a73cf15daeb3ea
jarek <jarek@debian.(none)>
2012-04-21 17:06:37 -0400

    get a random repo from a random user

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
   
-    print "<pre>";  
-  $repos = array('php', 'java', 'ruby', 'python', 'c', 'javascript');
+  print "<pre>";  
+  //$repos = array('php', 'java', 'ruby', 'python', 'c', 'javascript');
+  $repos = array('php');
+
   $usernames = array(); 
   foreach($repos as $repo){
     $users = "https://github.com/api/v2/json/repos/search/" . $repo;
@@ -13,22 +15,22 @@
     }
   }
 
-    print " we have a total of " . count($usernames) . " users";
-foreach($usernames as $user){
-    print "looking at $user\n";
-    get_random_file($user);
-}
+    print " we have a total of " . count($usernames) . " users\n";
+    $rand_user = $usernames[array_rand($usernames)];
+    print "We randomly are looking at the usr: $rand_user\n";
+    get_random_file($rand_user);
 
     print "</pre>";  
+
+
 
 function get_random_file($username){
     $repo_url = "https://api.github.com/users/" . $username . "/repos";
     $repos = json_decode(file_get_contents($repo_url));
-    foreach($repos as $key=>$val){
-        print "looking at $key=>";
-        print_r($val);
-        print "\n";
-    }
+    print "they have " . count($repos) . " different repos, we are randomly looking at: ";
+    $rand_repo = $repos[array_rand($repos)];
+    print $rand_repo->name . "\n";
+
 }
 
 ?>
